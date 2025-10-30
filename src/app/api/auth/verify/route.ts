decb4537ff89e6ff9e7f78bed0789271a3ad985c
Maheedhar <maheedharambadipudi756@gmail.com>
2025-10-30 13:32:23 +0530

    NextAuth implementation

--- src/app/api/auth/verify/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { verifyToken } from "../../../../lib/auth";
-
-export async function GET(){
-  const user = verifyToken();
-  if(!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  return NextResponse.json({ user })
-}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,29 @@
 import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default withAuth(
-  function middleware(req) {
-    
+  function middleware(req : NextRequest) {
+
+    const token = (req as any).nextauth?.token;
+    const role = token?.role as string | undefined;
+
+    if (req.nextUrl.pathname.startsWith("/admin") && role !== "admin") {
+      return NextResponse.redirect(new URL("/shop", req.url));
+    }
+
+    if (req.nextUrl.pathname.startsWith("/shop") && !token) {
+      return NextResponse.redirect(new URL("/(auth)/login", req.url));
+    }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }: { token: any }) => !!token
+    }
   }
-)
+);
+
+export const config = {
+  matcher: ["/shop/:path*", "/user/:path*", "/admin/:path*"]
+}
